d6de00377d835053b0b5f8aaa2351223824aa5e3
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2020-11-24 23:44:43 +0900

    practice-1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.component('my-component', {
+  data: function(){
+    return {
+      number: 12
+    }
+  },
+  template: '<p>いいね{{number}}</p>'
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
